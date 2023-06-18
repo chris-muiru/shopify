c4dd520b9f927fe98ac1fa829edc1910202b19f7
kris <krisdjangodeveloper@gmail.com>
2023-06-18 03:08:04 +0300

    update: add grid

--- app/layout/Footer.tsx
+++ app/layout/Footer.tsx
@@ -8,22 +8,27 @@ const Footer = () => {
 		<SectionWrapper>
 			<footer className="space-y-4 mt-20">
 				<hr className="" />
-				<div className="grid lg:grid-cols-5">
-					<div className="w-full flex flex-col space-y-6">
-						<div className="w-[200px] h-[50px] relative">
-							<Image
-								src={
-									"https://uploads-ssl.webflow.com/63e857eaeaf853471d5335ff/63e86ab4c21faa7bc0bd90dd_Logo.svg"
-								}
-								alt="companyLogo"
-								fill
-							/>
+				<div className="grid grid-cols-4 gap-6 sm:grid-cols-3 lg:grid-cols-5 lg:row-span-1">
+					<div className="col-span-4 sm:col-start-1 sm:col-span-2 lg:row-span-1 lg:col-span-2 lg:col-start-1 lg:row-start-1 space-y-3 lg:self-start">
+						<div>
+							<div className="w-[200px] h-[50px] relative">
+								<Image
+									src={
+										"https://uploads-ssl.webflow.com/63e857eaeaf853471d5335ff/63e86ab4c21faa7bc0bd90dd_Logo.svg"
+									}
+									alt="companyLogo"
+									fill
+								/>
+							</div>
+							<div>
+								Amet minim mollit non deserunt ullamco est sit aliqua
+								dolor do amet sint. Velit officia consequat duis enim
+								velit mollit.
+							</div>
 						</div>
-						<p>
-							Amet minim mollit non deserunt ullamco est sit aliqua dolor
-							do amet sint. Velit officia consequat duis enim velit
-							mollit.
-						</p>
+					</div>
+
+					<div className="row-start-5 col-span-4  sm:row-span-1 sm:col-span-2 lg:col-span-2 lg:col-start-1">
 						<p className="font-semibold">Accepted Payments</p>
 						<div className="flex flex-row flex-wrap gap-2">
 							<div className="border w-[60px] relative p-4 border-black rounded-md"></div>
@@ -36,7 +41,7 @@ const Footer = () => {
 							<div className="border w-[60px] relative p-4 border-black rounded-md"></div>
 						</div>
 					</div>
-					<div className="ml-10">
+					<div className="col-span-2 sm:col-span-1 sm:col-start-3 sm:row-span-3 sm:row-start-1  lg:row-span-full lg:col-span-1">
 						<p className="font-bold mb-3">DepartMent</p>
 						<p>Fasshion</p>
 						<p>Fasshion</p>
@@ -50,7 +55,7 @@ const Footer = () => {
 						<p>Fasshion</p>
 						<p>Fasshion</p>
 					</div>
-					<div>
+					<div className="col-span-2 sm:col-span-1 lg:row-span-full lg:col-span-1">
 						<p className="font-bold mb-3">About Us</p>
 
 						<p>Fasshion</p>
@@ -60,7 +65,7 @@ const Footer = () => {
 						<p>Fasshion</p>
 						<p>Fasshion</p>
 					</div>
-					<div>
+					<div className="col-span-2 sm:col-span-1  lg:row-span-full lg:col-span-1">
 						<p className="font-bold mb-3">Services</p>
 						<p>Fasshion</p>
 						<p>Fasshion</p>
@@ -68,7 +73,7 @@ const Footer = () => {
 						<p>Fasshion</p>
 						<p>Fasshion</p>
 					</div>
-					<div>
+					<div className="col-span-2 sm:col-span-1  lg:row-span-full lg:col-span-1">
 						<p className="font-bold mb-3">Help</p>
 						<p>Fasshion</p>
 						<p>Fasshion</p>
